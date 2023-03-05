5d9172622b27af13ca2cc2ae16e03c2edf7a48a4
소태호 <sth9057@gmail.com>
2023-03-05 18:33:56 +0900

    파이썬 깃 등록 테스트

---
+++ test.py
@@ -0,0 +1,15 @@
+a = "you need python %20.5f %d" % (1.2334512313, 123)
+num = 123
+b = f'you need python {num:_^11}'
+print(a)
+print(b)
+var = f'{"python":!^12}'
+print(f'{"python":!^12}')
+print(var)
+d1 = {1 : 'a', 2:'b', 3:'c'}
+print(d1.items())
+print(d1.get("13"))
+
+aa = bb = 'python'
+aa = "life"
+print(bb)
